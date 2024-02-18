318d4dd4570dc3480dc222478f8f39052ba7c95c
Evan Allen <eallen@xpirit.com>
2024-02-18 14:08:19 -0500

    init

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const { MessagingResponse } = require('twilio').twiml;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+router.post('/sms', (req, res) => {
+  const twiml = new MessagingResponse();
+  console.log("Hello")
+
+  twiml.message('The Robots are coming! Head for the hills!');
+
+  res.type('text/xml').send(twiml.toString());
 });
 
 module.exports = router;
